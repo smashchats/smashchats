a2ed100fc1874ba6f87f372df1eae714404e128b
mbp16 <z6Mkjzb5gwhWMEAhi3psM13oNuszoSSQb2rTgWgVwRdMwrQ3@dev.smashchats.com>
2024-12-18 12:16:45 +0100

    db reset

--- drizzle/0001_exotic_maddog.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `contacts` ADD `blocked` integer DEFAULT false;
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,24 +1,12 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_romantic_lake.sql';
-import m0001 from './0001_exotic_maddog.sql';
-import m0002 from './0002_numerous_swordsman.sql';
-import m0003 from './0003_hesitant_iron_man.sql';
-import m0004 from './0004_powerful_next_avengers.sql';
-import m0005 from './0005_hard_agent_brand.sql';
-import m0006 from './0006_lucky_wendell_vaughn.sql';
+import m0000 from './0000_tranquil_ogun.sql';
 
   export default {
     journal,
     migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005,
-m0006
+      m0000
     }
   }
   
