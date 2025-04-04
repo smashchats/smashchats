0c8ae18dc6b1305caf0a0e291545617c8f4b3861
mbp16 <z6Mkjzb5gwhWMEAhi3psM13oNuszoSSQb2rTgWgVwRdMwrQ3@dev.smashchats.com>
2025-04-04 17:31:08 -0400

    fix tests (again and again)

--- __tests__/ui/design-system/AvatarFallbackText-test.tsx
+++ __tests__/ui/design-system/AvatarFallbackText-test.tsx
@@ -5,12 +5,64 @@ describe("AvatarFallbackText", () => {
     test("Renders correctly", () => {
         const tree = render(<AvatarFallbackText name="test" />).toJSON();
 
-        expect(tree).toMatchSnapshot();
+        expect(tree).toMatchInlineSnapshot(`
+      <View
+        style={
+          {
+            "alignItems": "center",
+            "height": "100%",
+            "justifyContent": "center",
+            "width": "100%",
+          }
+        }
+      >
+        <Text
+          style={
+            {
+              "color": "white",
+              "fontSize": 24,
+              "fontWeight": "bold",
+            }
+          }
+        >
+          T
+        </Text>
+      </View>
+    `);
     });
 
     test("Renders correctly with no name", () => {
-        const tree = render(<AvatarFallbackText name={undefined as unknown as string} />).toJSON();
+        const tree = render(
+            <AvatarFallbackText name={undefined as unknown as string} />
+        ).toJSON();
 
-        expect(tree).toMatchSnapshot();
+        expect(tree).toMatchInlineSnapshot(`
+      <View
+        style={
+          {
+            "alignItems": "center",
+            "height": "100%",
+            "justifyContent": "center",
+            "width": "100%",
+          }
+        }
+      >
+        <Text
+          style={
+            {
+              "color": "white",
+              "fontSize": 24,
+              "fontWeight": "bold",
+            }
+          }
+        />
+      </View>
+    `);
+    });
+
+    test("Renders initials correctly", () => {
+        const { getByText } = render(<AvatarFallbackText name="John Doe" />);
+
+        expect(getByText("JD")).toBeTruthy();
     });
 });
