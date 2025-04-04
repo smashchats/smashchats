fbe96d6f85d6f35042e18aa5f2b3aa12c5103d87
mbp16 <z6Mkjzb5gwhWMEAhi3psM13oNuszoSSQb2rTgWgVwRdMwrQ3@dev.smashchats.com>
2025-04-04 17:33:30 -0400

    fix tests (again and again)

--- __tests__/ui/design-system/Checkbox-test.tsx
+++ __tests__/ui/design-system/Checkbox-test.tsx
@@ -3,13 +3,25 @@ import { render } from "@testing-library/react-native";
 import { Checkbox } from "@/src/ui/design-system/Checkbox";
 
 describe("Checkbox", () => {
-    it("renders correctly", () => {
-        const tree = render(<Checkbox checked={false} />).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders correctly", () => {
+    const tree = render(<Checkbox checked={false} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <View>
+        <Text>
+          circle-outline
+        </Text>
+      </View>
+    `);
+  });
 
-    it("renders correctly when checked", () => {
-        const tree = render(<Checkbox checked={true} />).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders correctly when checked", () => {
+    const tree = render(<Checkbox checked={true} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <View>
+        <Text>
+          check-circle
+        </Text>
+      </View>
+    `);
+  });
 });
