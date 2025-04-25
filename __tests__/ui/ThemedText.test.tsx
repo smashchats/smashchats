bef77c30ab3a39bb11d1c5cb27f35171de7f4e9c
mbp16 <z6Mkjzb5gwhWMEAhi3psM13oNuszoSSQb2rTgWgVwRdMwrQ3@dev.smashchats.com>
2025-04-25 17:10:49 +0200

    Add tests for ThemedText component with custom colors and types, and update snapshots
    - Introduced new test cases for ThemedText to validate rendering with custom light and dark colors.
    - Added tests for various text types including title, subtitle, link, default, and defaultSemiBold.
    - Updated snapshots to reflect the new test cases and ensure accurate rendering validation.

--- __tests__/ui/ThemedText.test.tsx
+++ __tests__/ui/ThemedText.test.tsx
@@ -8,3 +8,56 @@ it(`renders correctly`, () => {
 
     expect(tree).toMatchSnapshot();
 });
+
+describe("ThemedText with custom color", () => {
+    it(`renders correctly with a light theme custom color`, () => {
+        const tree = render(
+            <ThemedText lightColor="red" darkColor="blue">
+                Snapshot test!
+            </ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe("ThemedText with type", () => {
+    it(`renders correctly with type title`, () => {
+        const tree = render(
+            <ThemedText type="title">Snapshot test!</ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+    it(`renders correctly with type subtitle`, () => {
+        const tree = render(
+            <ThemedText type="subtitle">Snapshot test!</ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`renders correctly with type link`, () => {
+        const tree = render(
+            <ThemedText type="link">Snapshot test!</ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`renders correctly with type default`, () => {
+        const tree = render(
+            <ThemedText type="default">Snapshot test!</ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`renders correctly with type defaultSemiBold`, () => {
+        const tree = render(
+            <ThemedText type="defaultSemiBold">Snapshot test!</ThemedText>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
