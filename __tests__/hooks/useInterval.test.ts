52d271085c441c98c9517f845299d1c8cad68960
mbp16 <z6Mkjzb5gwhWMEAhi3psM13oNuszoSSQb2rTgWgVwRdMwrQ3@dev.smashchats.com>
2025-04-28 11:00:35 +0200

    make tests quicker

--- __tests__/hooks/useInterval.test.ts
+++ __tests__/hooks/useInterval.test.ts
@@ -2,27 +2,32 @@ import { renderHook } from "@testing-library/react-native";
 
 import useInterval from "@/src/hooks/useInterval";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 describe("useInterval", () => {
-    it("should call the callback after the delay", async () => {
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    it("should call the callback after the delay", () => {
         const callback = jest.fn();
         renderHook(() => useInterval(callback, 1000));
-        await sleep(1500);
+        jest.advanceTimersByTime(1500);
         expect(callback).toHaveBeenCalled();
     });
 
-    it("should not call the callback if the delay is null", async () => {
+    it("should not call the callback if the delay is null", () => {
         const callback = jest.fn();
         renderHook(() => useInterval(callback, null));
-        await sleep(1500);
+        jest.advanceTimersByTime(1500);
         expect(callback).not.toHaveBeenCalled();
     });
 
-    it("should call the callback twice after the delay", async () => {
+    it("should call the callback twice after the delay", () => {
         const callback = jest.fn();
         renderHook(() => useInterval(callback, 1000));
-        await sleep(2500);
+        jest.advanceTimersByTime(2500);
         expect(callback).toHaveBeenCalledTimes(2);
     });
 });
